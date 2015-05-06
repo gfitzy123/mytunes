bb75ea04769f911498c51753aa76cd8e5f561009
MKS-Student <mks.student@hackreactor.com>
2015-05-06 15:28:47 -0700

    enqueue trigger updating SongQueueView

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,11 +3,14 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td class="addToQueue"> Q </td>'),
 
   events: {
     'click': function() {
       this.model.play();
+    },
+    'click .addToQueue': function(){
+      this.model.enqueue();   
     }
   },
 
